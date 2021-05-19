07eef6d311f2ba604fb137ef0ed4ec5e2b1d90d1
lukasberk <lukasvdberk@gmail.com>
2021-05-19 12:47:49 +0200

    Created settings layout

--- app/src/main/java/com/example/youdrank/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/youdrank/ui/settings/SettingsFragment.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.youdrank.R;
 import com.example.youdrank.controllers.WaterIntakeController;
 import com.example.youdrank.databinding.FragmentHomeBinding;
 import com.example.youdrank.databinding.FragmentSettingsBinding;
@@ -33,6 +37,33 @@ public class SettingsFragment extends Fragment {
         waterIntakeController = new WaterIntakeController(getContext());
 
         return root;
+    }
+
+    public void setupListener() {
+        Button waterIntakeBtn = mView.findViewById(R.id.save_settings);
+        waterIntakeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    EditText dailyWaterIntakeSetting = mView.findViewById(R.id.water_intake_per_day);
+                    int waterIntake = Integer.parseInt(dailyWaterIntakeSetting.getText().toString());
+                } catch (Exception ignored) {
+                    showToast("You did not enter a valid number for water intake!");
+                }
+            }
+        });
+    }
+
+    private void saveSettings(int waterIntakePerDay) {
+
+    }
+
+    private void showToast(String text) {
+        Context context = getContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     @Override
